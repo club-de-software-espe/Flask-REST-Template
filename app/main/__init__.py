381215dc5f5ea9ec49a7ccc9b73597e96686273d
wabrborich@hotmail.com <wabrborich@hotmail.com>
2019-09-01 12:42:21 -0500

    add cors support

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from .service import urls
 from .plugins import db, ma
+from flask_cors import CORS
 
 
 def create_app(config_class):
@@ -8,6 +9,7 @@ def create_app(config_class):
     flask_app.config.from_object(config_class)
 
     __register_plugins(flask_app)
+    __cors_app(flask_app)
 
     return flask_app
 
@@ -16,3 +18,7 @@ def __register_plugins(flask_app):
     flask_app.register_blueprint(urls, url_prefix='/api')
     db.init_app(flask_app)
     ma.init_app(flask_app)
+
+
+def __cors_app(flask_app):
+    CORS(flask_app)
